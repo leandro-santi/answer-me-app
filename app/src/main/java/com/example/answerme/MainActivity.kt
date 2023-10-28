7f0d7bf8fb6f8031725b24a10bd6a418d4fd642b
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-10-28 02:10:57 -0400

    Created basic app interaction

--- app/src/main/java/com/example/answerme/MainActivity.kt
+++ app/src/main/java/com/example/answerme/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.answerme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var answerEditText: EditText
+    private lateinit var answerButton: Button
+    private lateinit var answerTextResult: TextView
+
+    private val answers = listOf("Yes", "No", "Maybe")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setViews()
+        setListener()
+
     }
+
+    private fun setViews() {
+
+        answerEditText = findViewById(R.id.et_question)
+        answerButton = findViewById(R.id.bt_makeQuestion)
+        answerTextResult = findViewById(R.id.tv_answer)
+
+    }
+
+    private fun setListener() {
+
+        answerButton.setOnClickListener {
+
+            answerQuestion()
+
+        }
+
+    }
+
+    private fun answerQuestion() {
+
+        val counter = Random.nextInt(3)
+        answerTextResult.text = answers[counter]
+
+    }
+
 }
